1920cd3af26662dc3736b6d07238c71aec6c8ff7
Luca Langella <langellaluca00@gmail.com>
2024-12-09 10:00:21 +0100

    Update .gitignore

---
+++ Ashtanga/Ashtanga/AshtangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AshtangaApp.swift
+//  Ashtanga
+//
+//  Created by Luca Langella 1 on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AshtangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ashtanga/Ashtanga/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ashtanga
+//
+//  Created by Luca Langella 1 on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
